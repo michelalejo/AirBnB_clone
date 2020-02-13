f126f228345eff0ff78a405f8a51cc7ee1f07d63
Feli <srgatoiscool@gmail.com>
2020-02-13 17:01:19 +0000

    Create BaseModel from dictionary

---
+++ models/base_model.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Simple class for base_model"""
+from datetime import datetime
+from uuid import uuid4
+
+
+class BaseModel:
+    """Class model"""
+
+    def __init__(self, *args, **kwargs):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """ should print: [<class name>] (<self.id>) <self.__dict__> """
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                      self.__dict__))
+
+    def save(self):
+        """ updates the public instance attribute
+        updated_at with the current datetime """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """ returns a dictionary containing
+        all keys/values of __dict__ of the instance """
+        dict = self.__dict__
+        dict['created_at'] = self.created_at.isoformat()
+        dict['updated_at'] = self.updated_at.isoformat()
+        dict['__class__'] = self.__class__.__name__
+        return(dict)
