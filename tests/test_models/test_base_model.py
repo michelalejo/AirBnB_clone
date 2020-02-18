99c832873630a37bbc6865e0bcc7023dbe84c5c8
michelalejo <michelmolina65@gmail.com>
2020-02-18 21:37:22 +0000

    tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Tests for the BaseModel class"""
+"""Tests for Base Model"""
 
 
 from models.base_model import BaseModel
@@ -10,7 +10,7 @@ import unittest
 
 
 class Testpep8(unittest.TestCase):
-    """Simple pep8 testing"""
+    """Pep8 testing"""
     def test_pep8(self):
         msg = "Found code style errors (and warning)."
         style = pep8.StyleGuide(quiet=True)
@@ -18,3 +18,13 @@ class Testpep8(unittest.TestCase):
         FileBase_ModelTest = "tests/test_models/test_base_model.py"
         check = style.check_files([FileBase_Model, FileBase_ModelTest])
         self.assertEqual(check.total_errors, 0, msg)
+
+    def id_unique(self):
+        """"Id testing"""
+
+        id_u = self._cls()
+        with self.subTest(msg='id is a UUID'):
+            self.assertIsInstance(id_u.id, str)
+            self.assertIsInstance(uuid.UUID(id_u.id), uuid.UUID)
+        with self.subTest(msg='IDs are unique'):
+            self.assertNotEqual(self.__class__().id, self.__class__().id)
