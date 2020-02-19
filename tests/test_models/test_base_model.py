341bc5d90d05ffc1ac72d4341537fed4b894719b
Feli <srgatoiscool@gmail.com>
2020-02-18 20:28:32 -0500

    adde some test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,6 +7,7 @@ import unittest
 import json
 import pep8
 import unittest
+from os import path
 
 
 class Testpep8(unittest.TestCase):
@@ -19,3 +20,36 @@ class Testpep8(unittest.TestCase):
         FileBase_ModelTest = "tests/test_models/test_base_model.py"
         check = style.check_files([FileBase_Model, FileBase_ModelTest])
         self.assertEqual(check.total_errors, 0, msg)
+
+
+class TestBase(unittest.TestCase):
+    """ Test all of i think """
+
+    def setUp(self):
+        """ Set a variable """
+        self.my_model = BaseModel
+
+    def tearDown(self):
+        pass
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def TestModels(self):
+        """Test model name"""
+        self.my_model.name = 'Holberton'
+        self.assertEqual(self.my_model.name, 'Holberton')
+        """Test model numbers"""
+        self.my_model.my_number = 55
+        self.assertEqual(self.my_model.my_number, 55)
+        """Test model exist"""
+        self.assertTrue(path.isfile('my_file.json'))
+
+
+if __name__ == '__main__':
+    unittest.main()
