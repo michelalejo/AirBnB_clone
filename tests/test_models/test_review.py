1a7d54bf3efc34530c99f70f72fe55f1ef5fa2ed
michelalejo <michelmolina65@gmail.com>
2020-02-20 01:44:06 +0000

    test_review.py

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""User tests"""
+
+
+from models.user import User
+from tests.test_models.test_base_model import TestBase
+import os
+import unittest
+
+class TestUser (TestBase):
+    """User tests"""
+
+    def __init__(self, *args, **kwargs):
+        """User tests"""
+
+        super().__init__(*args, **kwargs)
+        self._cls = User
+        self._name = 'User'
+
+    def test_permissions(self):
+        """User tests"""
+        is_read_true = os.access('models/user.py', os.R_OK)
+        self.assertTrue(is_read_true)
+        is_write_true = os.access('models/user.py', os.W_OK)
+        self.assertTrue(is_write_true)
+        is_exec_true = os.access('models/user.py', os.X_OK)
+        self.assertTrue(is_exec_true)
+
+
+    def test_instance(self):
+        """User tests"""
+        my_user = User()
+        self.assertIsInstance(my_user, User)
