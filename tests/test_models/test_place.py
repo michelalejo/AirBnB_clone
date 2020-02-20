0169d47d09b1d611b82e779d77689a0875d4fb9d
Feli <srgatoiscool@gmail.com>
2020-02-19 21:03:01 -0500

    test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
-"""User tests"""
+"""Place tests"""
 
 
-from models.user import User
+from models.place import Place
 from tests.test_models.test_base_model import TestBase
 import os
 import unittest
 
-class TestUser (TestBase):
-    """User tests"""
+class TestPlace (TestBase):
+    """Place tests"""
 
     def __init__(self, *args, **kwargs):
-        """User tests"""
+        """Place tests"""
 
         super().__init__(*args, **kwargs)
-        self._cls = User
-        self._name = 'User'
+        self._cls = Place
+        self._name = 'Place'
 
     def test_permissions(self):
-        """User tests"""
-        is_read_true = os.access('models/user.py', os.R_OK)
+        """Place tests"""
+        is_read_true = os.access('models/place.py', os.R_OK)
         self.assertTrue(is_read_true)
-        is_write_true = os.access('models/user.py', os.W_OK)
+        is_write_true = os.access('models/place.py', os.W_OK)
         self.assertTrue(is_write_true)
-        is_exec_true = os.access('models/user.py', os.X_OK)
+        is_exec_true = os.access('models/place.py', os.X_OK)
         self.assertTrue(is_exec_true)
 
 
     def test_instance(self):
-        """User tests"""
-        my_user = User()
-        self.assertIsInstance(my_user, User)
+        """Place tests"""
+        my_place = Place()
+        self.assertIsInstance(my_place, Place)
